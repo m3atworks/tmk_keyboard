939721f4b5870760d94bce7e1d5ebed0918a0871
m3atworks <m3atworks@gmail.com>
2020-04-18 17:18:09 +1200

    added my sgi

--- keyboard/alps64/keymap_common.h
+++ keyboard/alps64/keymap_common.h
@@ -48,7 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 }
 
 /* AEK US */
-#define KEYMAP_AEK( \
+#define KEYMAP_SGI( \
     K36, K37, K46, K47, K56, K57, K66, K67, K76, K77, K06, K07, K17, K27, \
     K34, K35, K44, K45, K54, K55, K64, K65, K75, K05, K15, K16, K25, K24, \
     K32, K33, K43, K52, K53, K63, K73, K74, K03, K04, K13, K14, K23, \
